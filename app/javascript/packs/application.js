9a36b69e987fd9683f518da17f651022e0849f9d
eslam <eslamelkholy5325762@gmail.com>
2020-04-21 01:37:52 +0200

    Fix jJquery Bug And Added Notifications UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,19 +2,19 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import 'bootstrap/dist/js/bootstrap';
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
-
-
+require("channels");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require popper
+//= require bootstrap
 //= require bootstrap-sprockets
