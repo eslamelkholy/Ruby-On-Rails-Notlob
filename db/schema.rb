511a6a83c7eb6d5ece5e3a82dc238fb8a7fa2f47
eslam <eslamelkholy5325762@gmail.com>
2020-04-24 11:42:48 +0200

    Group Friends

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_220232) do
+ActiveRecord::Schema.define(version: 2020_04_24_093423) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "group_friends", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
